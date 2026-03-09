6390f3df36a836dd329b24cbce4bc8c5a47e122e
Roman Reshetniak <romchik1110200l@gmail.com>
2026-03-09 15:11:41 +0000

    week3 javascript lab completed

---
+++ week3/script1.js
@@ -0,0 +1,12 @@
+document.write("Hello, world!<br>");
+document.write("This text is written using JavaScript.<br><br>");
+
+var name = "Roman";
+var age = 20;
+var student = true;
+
+document.write("Name: " + name + "<br>");
+document.write("Age: " + age + "<br>");
+document.write("Student: " + student + "<br>");
+
+console.log(name);
